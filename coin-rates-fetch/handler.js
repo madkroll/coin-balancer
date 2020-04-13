31ce0fccab52b83febadde90e2b80bbfdd6309c2
madkroll <mighty.boosta@gmail.com>
2020-04-13 17:14:38 +0200

    Read all rates from S3

--- coin-rates-fetch/handler.js
+++ coin-rates-fetch/handler.js
@@ -3,7 +3,7 @@
 const s3Bucket = require('./services/s3-bucket');
 
 module.exports.fetch = async function(event, context) {
-    const rates = JSON.stringify(await s3Bucket.loadReports())
+    const rates = JSON.stringify(await s3Bucket.loadReports());
 
     return {
         statusCode: 200,
--- coin-rates-fetch/services/s3-bucket.js
+++ coin-rates-fetch/services/s3-bucket.js
@@ -2,18 +2,28 @@ const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 
 const bucket = "coin-balancer";
-const ratesFolder = "rates";
 
 module.exports = {
 
     loadReports: async function () {
-        const s3Request = {
+        const listAllInBucket = {
             Bucket: bucket
         };
 
-        const rates = await s3.listObjectsV2(s3Request).promise();
-        console.log(rates);
-        return rates;
+        const storedRatesInfo = await s3.listObjectsV2(listAllInBucket).promise();
+
+        const foundRates = [];
+        for (let nextRateReference of storedRatesInfo.Contents) {
+            const loadGivenRate = {
+                Bucket: bucket,
+                Key: nextRateReference.Key
+            };
+
+            const rateContent = await s3.getObject(loadGivenRate).promise();
+            foundRates.push(JSON.parse(rateContent.Body.toString()));
+        }
+
+        return foundRates;
     }
 };
 
