b7ed8e4dd19af214601d8ec1d50fffe733fdefdf
madkroll <mighty.boosta@gmail.com>
2020-04-14 21:40:32 +0200

    Fetch rates for last 10 hours only

--- coin-rates-fetch/services/s3-bucket.js
+++ coin-rates-fetch/services/s3-bucket.js
@@ -2,18 +2,36 @@ const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 
 const bucket = "coin-balancer";
+const thresholdInMs = 10 * 60 * 60 * 1000;
 
 module.exports = {
 
     loadReports: async function () {
+        const since = Number(Date.now() - thresholdInMs);
+
         const listAllInBucket = {
-            Bucket: bucket
+            Bucket: bucket,
+            Delimiter: '/',
+            Prefix: 'rates/'
         };
 
         const storedRatesInfo = await s3.listObjectsV2(listAllInBucket).promise();
 
+        console.log(storedRatesInfo);
+
         const foundRates = [];
         for (let nextRateReference of storedRatesInfo.Contents) {
+            let rateTimestamp = Number(
+                nextRateReference.Key
+                    .replace("rates/", "")
+                    .split("-")[0]
+            );
+
+            if (rateTimestamp - since < 0) {
+                // skip old records
+                continue;
+            }
+
             const loadGivenRate = {
                 Bucket: bucket,
                 Key: nextRateReference.Key
