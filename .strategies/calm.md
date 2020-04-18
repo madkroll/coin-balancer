466f5e1d39ae951ace0dd50ae3e1520005318418
madkroll <mighty.boosta@gmail.com>
2020-04-18 18:23:22 +0200

    Define strategy: Calm

--- .strategies/calm.md
+++ .strategies/calm.md
@@ -1,10 +1,19 @@
 # Strategy: Calm
 
 ## Idea
+When market is calm - buy trusted coins and then wait for a CG to grow to sell on higher CG rates.
+Ignore CG drop, just wait.
+
+## Goal
+Increase CG value
 
 ## When
+- 3 days CG price stays in threshold -10% / + 10% (no growth / no drop)
 
 ## What
-
-- build baselines for tradable coins
-- set sell and buy targets
+- open new trades in trusted coins
+- for each trade - CG baseline
+- value structure: CG 30%, 70% equally spread across trusted coins
+- set CG targets / alerts
+- monitor CG rate growth, send alert if close to CG target
+- when target reached and crossed - track growth (high frequency) and convert everything to CG if any drop appears
--- coin-client/src/currencies.js
+++ coin-client/src/currencies.js
@@ -1,35 +1,36 @@
 const COLORS = {
-    // high waves
-    "XLM": "rgb(0,100,11)",
-    "ETH": "rgb(0,180,36)",
-    "ATOM": "rgb(0,217,40)",
-    "EOS": "rgb(100,173,0)",
-    "LINK": "rgba(113,148,0,0.76)",
-
-    // noise
-    "BAT": "rgba(148,143,143,0.76)",
-    "BCH": "rgba(148,143,143,0.76)",
-    "DASH": "rgba(148,143,143,0.76)",
-    "ETC": "rgba(148,143,143,0.76)",
-    "KNC": "rgba(148,143,143,0.76)",
-    "LTC": "rgba(148,143,143,0.76)",
-    "REP": "rgba(148,143,143,0.76)",
-    "XRP": "rgba(148,143,143,0.76)",
-    "ZEC": "rgba(148,143,143,0.76)",
-    "ZRX": "rgba(148,143,143,0.76)",
-
-
-    "XTZ": "rgba(0,0,0,0.76)",
-
-    // pumpers
-    "OXT": "rgba(109,0,148,0.76)",
 
     // btc baseline
     "BTC": "rgba(213,25,0)",
 
     // stablecoins and currencies
     "DAI": "rgb(213,25,0)",
-    "EUR": "rgba(255, 0, 0, 1)"
+    "EUR": "rgba(255, 0, 0, 1)",
+
+    // trusted and promising
+    "XTZ": "rgba(0,0,0,0.76)",
+    "ETH": "rgb(0,180,36)",
+    "LINK": "rgba(113,148,0,0.76)",
+    "ZEC": "rgba(208,134,17,0.76)",
+    "DASH": "rgba(28,108,226,0.76)",
+
+    // high waves
+    "XLM": "rgb(0,100,11)",
+    "ATOM": "rgb(0,217,40)",
+    "EOS": "rgb(100,173,0)",
+
+    // noise
+    "BAT": "rgba(148,143,143,0.76)",
+    "BCH": "rgba(148,143,143,0.76)",
+    "ETC": "rgba(148,143,143,0.76)",
+    "KNC": "rgba(148,143,143,0.76)",
+    "LTC": "rgba(148,143,143,0.76)",
+    "REP": "rgba(148,143,143,0.76)",
+    "XRP": "rgba(148,143,143,0.76)",
+    "ZRX": "rgba(148,143,143,0.76)",
+
+    // scam
+    "OXT": "rgba(109,0,148,0.76)"
 };
 
 export default {
