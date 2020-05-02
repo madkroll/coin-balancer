ec91eeb11e8c9db66cac228dc8609b29e0dd5860
madkroll <mighty.boosta@gmail.com>
2020-05-02 12:21:46 +0200

    Continue read pages of rates

--- coin-rates-fetch/services/s3-bucket.js
+++ coin-rates-fetch/services/s3-bucket.js
@@ -2,6 +2,7 @@ const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 
 const bucket = "coin-balancer";
+const prefix = "rates/";
 const millisecondsInHour = 60 * 60 * 1000;
 const baseline = "1587162997926";
 
@@ -13,31 +14,44 @@ module.exports = {
         const listAllInBucket = {
             Bucket: bucket,
             Delimiter: '/',
-            Prefix: 'rates/'
+            Prefix: prefix
         };
 
-        const storedRatesInfo = await s3.listObjectsV2(listAllInBucket).promise();
+        let foundRates = [];
+        let storedRatesInfo = await s3.listObjectsV2(listAllInBucket).promise();
+        foundRates = foundRates.concat(await readPage(storedRatesInfo.Contents, since));
 
-        const foundRates = [];
-        for (let nextRateReference of storedRatesInfo.Contents) {
-            const rateName = nextRateReference.Key.replace("rates/", "");
-            const rateTimestamp = Number(rateName.split("-")[0]);
-
-            if (!rateName.startsWith(baseline) && rateTimestamp - since < 0) {
-                // skip old records
-                continue;
-            }
-
-            const loadGivenRate = {
-                Bucket: bucket,
-                Key: nextRateReference.Key
-            };
-
-            const rateContent = await s3.getObject(loadGivenRate).promise();
-            foundRates.push(JSON.parse(rateContent.Body.toString()));
+        while(storedRatesInfo.IsTruncated) {
+            listAllInBucket.ContinuationToken = storedRatesInfo.NextContinuationToken;
+            storedRatesInfo = await s3.listObjectsV2(listAllInBucket).promise();
+            foundRates = foundRates.concat(await readPage(storedRatesInfo.Contents, since));
         }
 
         return foundRates;
     }
 };
+
+async function readPage(pageContent, since) {
+    const foundRates = [];
+
+    for (let nextRateReference of pageContent) {
+        const rateName = nextRateReference.Key.replace(prefix, "");
+        const rateTimestamp = Number(rateName.split("-")[0]);
+
+        if (!rateName.startsWith(baseline) && rateTimestamp - since < 0) {
+            // skip old records
+            continue;
+        }
+
+        const loadGivenRate = {
+            Bucket: bucket,
+            Key: nextRateReference.Key
+        };
+
+        const rateContent = await s3.getObject(loadGivenRate).promise();
+        foundRates.push(JSON.parse(rateContent.Body.toString()));
+    }
+
+    return foundRates;
+}
 
